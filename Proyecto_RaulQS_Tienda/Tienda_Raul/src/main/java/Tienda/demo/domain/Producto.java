d2b6a8333819719ca55cfdf9c56a5be35d929dff
Rauw000 <136671393+Rauw000@users.noreply.github.com>
2025-11-17 22:48:59 -0600

    Se actualiza las clases

---
+++ Proyecto_RaulQS_Tienda/Tienda_Raul/src/main/java/Tienda/demo/domain/Producto.java
@@ -0,0 +1,47 @@
+package Tienda.demo.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "producto")
+public class Producto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id_producto")
+    private Integer IdProducto;
+    
+    
+    @Column(nullable = false, length = 50)
+    @NotBlank(message = "La descripción no puede estar vacia.")
+    @Size(max = 50, message = "La descripción no puede tener mas de 50 caracteres.")
+    private String descripcion;
+    
+    @Column (columnDefinition = "TEXT")
+    private String detalle;
+    
+    @Column (precision = 12, scale = 2)
+    @NotNull(message = "El precio no puede estar vacio.")
+    @DecimalMin(value = "0.01", inclusive = true, message = "El precio debe ser mayor a 0.")
+    private BigDecimal precio;
+    
+    @NotNull(message = "El campo de existencias no puede estar vacio.")
+    @Min(value = 0, message = "Las existencias deben ser un numero mayor o igual a 0.")
+    private Integer existencias;
+    
+    @Column(name = "ruta_imagen", length = 1024)
+    private String rutaImagen;
+    private boolean activo;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_Categoria")
+    private Categoria catrgoria;
+    
+}
---
+++ Proyecto_RaulQS_Tienda/Tienda_Raul/src/main/java/Tienda/demo/services/ProductoService.java
@@ -0,0 +1,81 @@
+package Tienda.demo.services;
+
+import Tienda.demo.domain.Producto;
+import Tienda.demo.repository.ProductoRepository;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activo) {
+        if (activo) {
+            return productoRepository.findByActivoTrue();
+        } else {
+            return productoRepository.findAll();
+        }
+
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Producto> getProducto(Integer idProducto) {
+        return productoRepository.findById(idProducto);
+    }
+
+    @Transactional
+    public void save(Producto producto, MultipartFile imagenFile) {
+        producto = productoRepository.save(producto);
+        if (!imagenFile.isEmpty()) {
+            File tempFile = null;
+            try {
+                // Crear archivo temporal
+                tempFile = File.createTempFile("temp", imagenFile.getOriginalFilename());
+                imagenFile.transferTo(tempFile);
+
+                // Subir archivo a Firebase
+                String rutaImagen = firebaseStorageService.uploadFile(
+                        tempFile, "producto",
+                        String.valueOf(producto.getIdProducto()));
+
+                // Guardar la URL en la entidad
+                producto.setRutaImagen(rutaImagen);
+                productoRepository.save(producto);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                // Eliminar archivo temporal
+                if (tempFile != null && tempFile.exists()) {
+                    tempFile.delete();
+                }
+            }
+        }
+    }
+
+    @Transactional
+    public void delete(Integer idProducto) {
+        if (!productoRepository.existsById(idProducto)) {
+            throw new IllegalArgumentException("La producto con ID " + idProducto + " no existe.");
+        }
+        try {
+            productoRepository.deleteById(idProducto);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("No se puede eliminar el producto. Tiene datos asociados.", e); // Agregué la coma faltante
+        }
+    }
+}
+
